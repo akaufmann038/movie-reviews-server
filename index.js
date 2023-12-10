830ba1527cb50137e04efd117968eca7a059cf63
Alexander Kaufmann <alexkaufmann@Alexanders-MacBook-Pro.local>
2023-12-10 18:48:12 -0500

    change url

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ import LoginRoutes from "./Login/routes.js";
 import ProfileRoutes from "./Profile/routes.js";
 import DetailsRoutes from "./Details/routes.js";
 import TestRoutes from "./Test/routes.js";
-import session from "express-session"
+import session from "express-session";
 
 const app = express();
 const port = 3000;
 
-app.use(cors({
-  credentials: true,
-  origin: "http://localhost:3001",
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://splendorous-granita-a0f737.netlify.app/",
+  })
+);
 const sessionOptions = {
   secret: "any string",
   resave: false,
